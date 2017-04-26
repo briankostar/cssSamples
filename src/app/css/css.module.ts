9ed1379436d18d96a52603051dff3256abb0ebe4
briankostar <brian.kostar@gmail.com>
2017-04-26 00:27:30 -0400

    '.'

--- src/app/css/css.module.ts
+++ src/app/css/css.module.ts
@@ -5,25 +5,28 @@ import { CssComponent } from './css.component';
 import { BasicsComponent } from './basics/basics.component';
 import { FloatComponent } from './float/float.component';
 import { FlexComponent } from './flex/flex.component';
+import { PositionsComponent } from './positions/positions.component';
 
-const Router: ModuleWithProviders = RouterModule.forChild( [
-    { path:'css', component:CssComponent },
-    { path:'css/basics', component:BasicsComponent },
-    { path:'css/float', component:FloatComponent },
-    { path:'css/flex', component:FlexComponent }
+const Router:ModuleWithProviders = RouterModule.forChild( [
+	{ path:'css', component:CssComponent },
+	{ path:'css/basics', component:BasicsComponent },
+	{ path:'css/float', component:FloatComponent },
+	{ path:'css/flex', component:FlexComponent },
+	{ path:'css/positions', component:PositionsComponent }
 ] );
 
 @NgModule( {
-    imports     :[
-        CommonModule,
-        Router
-    ],
-    declarations:[
-        CssComponent,
-        BasicsComponent,
-        FloatComponent,
-        FlexComponent
-    ]
+	imports     :[
+		CommonModule,
+		Router
+	],
+	declarations:[
+		CssComponent,
+		BasicsComponent,
+		FloatComponent,
+		FlexComponent,
+		PositionsComponent
+	]
 } )
 export class CssModule {
 }
